3cee7d5a19fab0556a0b639c3ec44451568a8ea0
Dale Holstein <DaleHolstein@wifi_stu-10-38-66-24.uncc.edu>
2017-11-02 17:30:47 -0400

    added country for omdb

--- liri.js
+++ liri.js
@@ -139,6 +139,7 @@ function omdbFunc(movie) {
             console.log("Release Year: " + JSON.parse(body).Year);
             console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
             console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
             console.log("Language: " + JSON.parse(body).Language);
             console.log("Plot: " + JSON.parse(body).Plot);
             console.log("Actors: " + JSON.parse(body).Actors);
